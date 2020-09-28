e5f057a09e58e1793986528eced1b19ca22a1f8e
boblemarin <emeric@minimal.be>
2020-09-28 14:04:39 +0200

    Add img styling feedback when choosing target

--- extension/content.js
+++ extension/content.js
@@ -1,7 +1,17 @@
+var styleEl = document.createElement('style');
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-      document.body.style.cursor = "crosshair";
+      // TODO: insertRule into style sheet to give better visual feedback
+      // document.head.appendChild(styleEl);
+      styleEl = document.createElement('style');
+      document.head.appendChild(styleEl);
+      var styleSheet = styleEl.sheet;
+      styleSheet.insertRule('img:hover { border: 15px dotted red; cursor: crosshair; box-sizing: border-box;}', 0);
+
+
+      //document.body.style.cursor = "crosshair";
       document.addEventListener("mousedown", onClickInDocument);
     }
   }
@@ -9,8 +19,9 @@ chrome.runtime.onMessage.addListener(
 
 function onClickInDocument(event) {
   if (event.target.tagName == 'IMG' ){
-    document.body.style.cursor = 'default';
+    //document.body.style.cursor = 'default';
     document.removeEventListener("mousedown", onClickInDocument);
+    document.head.removeChild(styleEl);
     console.log(event.target.src);
     chrome.runtime.sendMessage({"message": "open_new_tab", "url": event.target.src});
   }
